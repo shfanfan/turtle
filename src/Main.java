62ec7f283a32d6f6c4d4924dbdcbb60074b1ec4f
aviv harari <shfanfan@yahoo.com>
2024-09-11 22:39:51 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import unit4.turtleLib.Turtle;
+
+public class Main {
+    
+	public static void main(String[] args)
+  {
+
+    Turtle t1 = new Turtle();
+    t1.tailDown();
+    t1.moveForward(100);
+    t1.turnRight(90);
+    t1.moveForward(200);
+   
+  }
+}
